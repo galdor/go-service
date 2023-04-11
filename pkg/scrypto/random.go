8538b2dd786b893f927402f4d3d27b1b15f0b18f
Nicolas Martyanoff <nicolas@n16f.net>
2023-04-11 16:05:30 +0200

    add scrypto.RandomBytes

---
+++ pkg/scrypto/random.go
@@ -0,0 +1,17 @@
+package scrypto
+
+import (
+	"crypto/rand"
+
+	"github.com/galdor/go-service/pkg/utils"
+)
+
+func RandomBytes(n int) []byte {
+	data := make([]byte, n)
+
+	if _, err := rand.Read(data); err != nil {
+		utils.Panicf("cannot generate random data: %w", err)
+	}
+
+	return data
+}
