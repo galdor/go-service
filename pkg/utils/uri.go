65c6ca78916c45de7caa0f30c416368b41c128c3
Nicolas Martyanoff <nicolas@n16f.net>
2024-05-20 11:40:12 +0200

    add utils.URIMerge

--- pkg/utils/uri.go
+++ pkg/utils/uri.go
@@ -5,8 +5,40 @@ import (
 	"path"
 )
 
+func URIMerge(base, ref *url.URL) *url.URL {
+	uri := *base
+
+	if ref.Scheme != "" {
+		uri.Scheme = ref.Scheme
+	}
+
+	if ref.Host != "" {
+		uri.Host = ref.Host
+	}
+
+	if ref.User != nil {
+		uri.User = Ref(*ref.User)
+	}
+
+	if ref.Path != "" {
+		uri.Path = path.Join(base.Path, ref.Path)
+	}
+
+	query := base.Query()
+	for name, values := range ref.Query() {
+		for _, value := range values {
+			query.Add(name, value)
+		}
+	}
+	uri.RawQuery = query.Encode()
+
+	if ref.Fragment != "" {
+		uri.Fragment = ref.Fragment
+	}
+
+	return &uri
+}
+
 func URIMergePath(uri *url.URL, uriPath string) *url.URL {
-	uri2 := *uri
-	uri2.Path = path.Join(uri2.Path, uriPath)
-	return &uri2
+	return URIMerge(uri, &url.URL{Path: uriPath})
 }
