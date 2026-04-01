c9b4381b669d45df6209d9eec6b23e295bf52ab4
Nicolas Martyanoff <nicolas@n16f.net>
2026-04-01 12:06:26 +0200

    add the env2 template function

--- pkg/stemplate/functions.go
+++ pkg/stemplate/functions.go
@@ -1,6 +1,7 @@
 package stemplate
 
 import (
+	"cmp"
 	"encoding/json"
 	"os"
 	"strings"
@@ -8,6 +9,10 @@ import (
 
 var Funcs = map[string]interface{}{
 	"env": os.Getenv,
+
+	"env2": func(name, defaultValue string) string {
+		return cmp.Or(os.Getenv(name), defaultValue)
+	},
 
 	"quote": func(s string) string {
 		data, _ := json.Marshal(s)
